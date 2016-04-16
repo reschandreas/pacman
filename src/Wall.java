a7ebeb5a35f3b4663905b181d91cb4fd98a7bb9d
andreas_resch <resch.andreas@hotmail.it>
2016-04-16 21:10:45 +0200

    improved the getting images method

--- src/Wall.java
+++ src/Wall.java
@@ -1,5 +1,9 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
 
 /**
@@ -14,31 +18,19 @@ public class Wall extends JComponent {
 
     public Wall(String path) {
         image_path = path;
-        URL url = this.getClass().getResource(path);
-        if (url == null)
-            System.out.println("File not Found");
-        else {
-            image = getToolkit().getImage(url);
-            prepareImage(image, this);
-            Thread t = Thread.currentThread();
-            // Warte bis die Eigenschaften des Bildes geladen sind
-            while ((checkImage(image, this) & PROPERTIES) != PROPERTIES) {
-                try {
-                    // Pause, um dem Ladevorgang keine Ressourcen zu nehmen
-                    Thread.sleep(50);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-            // Stelle Größe des Objektes auf Größe des Bildes ein
-            this.setSize(this.image.getWidth(this), this.image.getHeight(this));
+        try {
+            image = ImageIO.read(new File(getClass().getResource(path).toURI()));
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
         }
+        // Stelle Größe des Objektes auf Größe des Bildes ein
+        this.setSize(this.image.getWidth(this), this.image.getHeight(this));
     }
-
     /**
      * Methode, welche automatisch aufgerufen wird und das Bild in der Größe des Objektes
      * darstellt
      */
+
     public void paint(Graphics g) {
         if (image != null) {
             g.drawImage(this.image, 0, 0, this);
